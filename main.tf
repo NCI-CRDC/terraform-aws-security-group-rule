c287fdba33b9c0db59f77c13b6368c6ea3e52568
Cole DeVries <colemandevries@gmail.com>
2023-02-05 10:38:33 -0500

    removed the prefix_list_ids variable as its rarely used

--- main.tf
+++ main.tf
@@ -4,7 +4,6 @@ resource "aws_security_group_rule" "this" {
   description              = var.description
   from_port                = var.from_port
   to_port                  = var.to_port
-  prefix_list_ids          = [var.prefix_list_ids]
   protocol                 = var.protocol
   self                     = var.self
   source_security_group_id = var.source_security_group_id
